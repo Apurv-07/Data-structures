624d811661fe8c990e78b732279c724396ecb27f
Apurv-07 <mailapurva007@gmail.com>
2023-01-16 16:41:59 +0530

    Program 1

---
+++ Counter.js
@@ -0,0 +1,15 @@
+//This program will check if the array 2 has all the elements twice f array one or not and returns true/false
+var c=[3,7, 5,11, 2];
+var d=[14, 6, 5, 22, 10];
+
+function Counter(a,b){
+  for (var i of a){
+    if(b.includes(i*2)){
+      console.log("Yes "+i)
+    }else{
+      return false
+    }
+  }
+  return true
+}
+console.log(Counter(c,d))
